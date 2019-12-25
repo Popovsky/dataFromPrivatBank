4820af34c48db733d43120d4f21eeac0863385de
pad19 <pad19051999@gmail.com>
2019-12-25 15:34:28 +0200

    init commit

--- js/app.js
+++ js/app.js
@@ -2,8 +2,23 @@ const data = fetch('https://popovsky.github.io/dataFromPrivatBank/data.json');
 data
     .then(res => res.json())
     .then(res => {
-        console.log('responce', res.arr);
+        let arr = getUniqTags(res.devices.map(el => el.placeRu));
+        let obj = new Object();
+        let arrOfObj = [];
+        arr.forEach(el => {
+            arrOfObj = [];
+            res.devices.forEach(e => el === e.placeRu ? arrOfObj.push(e) : null);
+            obj[el] = arrOfObj;
+        });
+        console.log(obj);
     })
     .catch(error => {console.log('error', error)});
 
-// console.log('devices SYNC', devices);
+const getUniqTags = tags => {
+    let results = [];
+    tags.forEach(value => {
+        value = value.trim();
+        results.indexOf(value) === -1 ? results.push(value) : null;
+    });
+    return results;
+};
